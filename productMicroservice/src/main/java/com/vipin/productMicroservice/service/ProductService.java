7ba387f439b31bdf1b75a4802dade91b2c39f931
vipin <vipinjsharma26@gmail.com>
2025-03-08 22:58:35 +0530

    Revert "kafka project with idempotent producer and consumer"
    This reverts commit aad17cc9edebe12d70bbc02219093fa259525f73.

--- productMicroservice/src/main/java/com/vipin/productMicroservice/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.vipin.productMicroservice.service;
-
-import com.vipin.productMicroservice.dto.ProductRequest;
-import com.vipin.productMicroservice.dto.ProductResponse;
-
-public interface ProductService {
-    ProductResponse createProduct(ProductRequest product);
-}
